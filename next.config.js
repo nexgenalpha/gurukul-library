4e3fadf80ff0db14dc25bf7d1be7b57decf3d35b
nexgenalpha <nexgenalpha26@gmail.com>
2026-09-08 13:53:06 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  trailingSlash: true,
 
   basePath: "/gurukul-library",
   assetPrefix: "/gurukul-library/",
 
-  trailingSlash: true,
-
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "*.supabase.co",
-        pathname: "/storage/v1/object/public/**",
-      },
-    ],
   },
 
   eslint: {
     ignoreDuringBuilds: true,
-  },
-};
+},
 
 module.exports = nextConfig;
